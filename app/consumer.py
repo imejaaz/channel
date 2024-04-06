237f54202562176e42b6e5b881bf140682deb877
imejaaz <waiting.amail@gmail.com>
2024-04-06 20:51:06 +0500

    group message enabled

--- app/consumer.py
+++ app/consumer.py
@@ -1,21 +1,49 @@
 from channels.consumer import SyncConsumer
-
+from asgiref.sync import async_to_sync
+from channels.exceptions import StopConsumer
+import json
 class sconsumer(SyncConsumer):
-
     def websocket_connect(self, event):
         self.send({
             "type": "websocket.accept",
         })
         print('connected')
+        async_to_sync(self.channel_layer.group_add)(
+            'programmers',
+            self.channel_name
+        )
+        print('channel name......',self.channel_name)
+
 
     def websocket_receive(self, event):
-        self.send({
-            "type": "websocket.send",
-            "text": event["text"],
+
+        message = event['text']
+        print(message)
+        async_to_sync(self.channel_layer.group_send)(
+        
+        'programmers',
+        {
+            'type':'chat.message',
+            'message':event['text']
         })
+    
+    def chat_message(self, event):
+        self.send({
+            'type':'websocket.send',
+            'text':event['message']
+        })
+    
 
 
-
+    def websocket_disconnect(self, event):
+        print("connection disconnected...", event)
+        print('channel layer is...', self.channel_layer)
+        print('channel name is...', self.channel_name)
+        async_to_sync(self.channel_layer.group_discard)(
+            'programmers',
+            self.channel_name
+        )
+        raise StopConsumer()
 
 
 
